5a4ebba65e9a1d980641c966a7765b1068290261
sherm3 <mamafiendaasd@gmail.com>
2022-12-04 21:23:18 +0300

    t5

---
+++ task5.py
@@ -0,0 +1,33 @@
+'''
+Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+
+Пример:
+
+- для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+'''
+
+def fibbonachi():
+    number = int(input("Введите число: "))
+    fibo = []
+    sum_up = 0
+    num1 = 1
+    num2 = 1
+    
+    for i in range(number):
+        fibo.append(num1)
+        sum_up = num1 + num2
+        num1 = num2
+        num2 = sum_up
+    
+    num1 = 0
+    num2 = 1
+    
+    for i in range(number + 1):
+        fibo.insert(0, num1)
+        sum_up = num1 - num2
+        num1 = num2
+        num2 = sum_up
+
+    print(fibo)
+
+fibbonachi()
